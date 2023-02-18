3a2864baa605af8c56d5dbc430efa8030fee135f
LOKESH <32417590+lokeshagg13@users.noreply.github.com>
2023-02-18 21:10:32 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: "",
+      nader: "nader"
+    };
+    this.coinToss = this.coinToss.bind(this);
+  }
+  coinToss() {
+    this.setState({ nader: "" }, () => {
+      if (Math.random() < 0.5) {
+        this.setState({ result: "heads" });
+        console.log("heads");
+      } else {
+        this.setState({ result: "tails" });
+        console.log("tails");
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="coin" className={this.state.result} key={+new Date()}>
+          <div class="side-a">
+            <h2>TAIL</h2>
+          </div>
+          <div className="side-b">
+            <h2>HEAD</h2>
+          </div>
+        </div>
+        <h1>Flip a coin</h1>
+        <button id="btn" onClick={this.coinToss}>
+          Coin Toss
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
